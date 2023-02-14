930aaadddc16f4b8ec6e8e2416aeac36426886d3
rs-rahamn <jesminakter.rahaman@gmail.com>
2023-02-14 23:20:45 +0600

    rhoeho;oahf

--- script/main.js
+++ script/main.js
@@ -1,15 +1,15 @@
 // Home
 document.getElementById("addMoney").addEventListener("click", function () {
-  window.location.href = "./script/addMoney.js";
+  window.location.href = "../atherPages/addMoney.html";
 });
 document.getElementById("saving").addEventListener("click", function () {
-  window.location.href = "saving.html";
+  window.location.href = "../atherPages/saving.html";
 });
 document.getElementById("sendMoney").addEventListener("click", function () {
-  window.location.href = "sendMoney.html";
+  window.location.href = "../atherPages/sendMoney.html";
 });
 document.getElementById("foundTransfer").addEventListener("click", function () {
-  window.location.href = "foundTransfer.html";
+  window.location.href = "../atherPages/foundTransfer.html";
 });
 document.getElementById("cashOut").addEventListener("click", function () {
   window.location.href = "cashOut.html";
@@ -17,8 +17,8 @@ document.getElementById("cashOut").addEventListener("click", function () {
 document
   .getElementById("mobileRecharge")
   .addEventListener("click", function () {
-    window.location.href = "mobileRecharge.html";
+    window.location.href = "../atherPages/mobileRecharge.html";
   });
 document.getElementById("donation").addEventListener("click", function () {
-  window.location.href = "donation.html";
+  window.location.href = "../atherPages/donation.html";
 });
